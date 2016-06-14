f6af1a0ccf3b91a8b956393e5ce17e4f90bb6f52
Jd Lausingco <jd.lausingco.jl@gmail.com>
2016-06-14 11:10:45 +0800

    Wordpress

---
+++ wp-content/themes/wpbootstrap/clinical.php
@@ -0,0 +1,224 @@
+<?php?>
+    <!DOCTYPE html>    
+    <html lang="en">
+
+    <head>
+     <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <title>Cardinal Santos Medical Center</title>
+      <!-- Bootstrap Core CSS -->
+      <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet"> 
+      <!-- Custom Fonts -->
+      <?php wp_enqueue_script("jquery"); ?>
+      <?php wp_head(); ?>
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'> 
+      <link rel="stylesheet" href="bootstrap/includes/themes/cardinal123.min.css"/>
+      <link rel="stylesheet" href="bootstrap/includes/jquery.mobile-1.1.1/jquery.mobile.structure-1.1.1.min.css" /> 
+      <link rel="stylesheet" href="bootstrap/includes/cssoverrides.css"/>  
+    <style>
+
+    p.italic {
+        font-style: italic;
+    } 
+    </style>
+    </head>  
+        <body>
+            <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button> 
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right"> 
+                            <li>
+                                <a class="page-scroll" href="#services">Services</a>
+                            </li> 
+                            <li>
+                                <a class="page-scroll" href="#labresultsonline">Lab Results Online</a>
+                            </li> 
+                            <li>
+                                <a class="page-scroll" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>  
+                </div> 
+            </nav>
+            <header>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="1500">
+                    <br>
+                    <br>
+                    <h1>Clinical Departments</h1> 
+                    <br>
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                    </ol> 
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="bootstrap/img/clinical/11.png" class="img-responsive" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="bootstrap/img/clinical/22.png" class="img-responsive" alt=""> 
+                        </div> 
+                        <div class="item">
+                            <img src="bootstrap/img/clinical/33.png" class="img-responsive" alt="">
+                        </div>
+                        <div class="item">
+                            <img src="bootstrap/img/clinical/44.png" class="img-responsive" alt="">
+                        </div> 
+                    </div> 
+                </div>
+                <br>
+                    <p class="italic">Cardinal Santos Medical Center prides itself on providing intelligent medical care. Through CSMC’s Continuing Medical Education programs, we continually expand the knowledge and expertise of our medical professionals.</p>
+                    <br>
+            </header>
+        <div data-role="content" style="font-size:.8em" data-theme="b">    
+        <div data-role="collapsible-set">
+                    <div data-role="collapsible">
+                        <h1>Anesthesiology</h1>
+                            <b>Our practice emphasizes patient safety as well as the highest level of skill in the science and art of Anesthesia. We believe in employing best practices in the delivery of our services.</b></br> 
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Dental Medecine</h1>
+                            <b>The Department of Dental Medicine of Cardinal Santos Medical Center will definitely make you smile. It offers comprehensive dental services and is fully dedicated to the patient’s comfort and satisfaction. It offers:</b><br>
+                            <li>General Dentistry</li>
+                            <li>Cosmetic Dentistry</li>
+                            <li>Endodontics (root canal therapy)</li>
+                            <li>Prosthodontics (dentures and bridges)</li>
+                            <li>Pediatric Dentistry</li>
+                            <li>Periodontics (diseases of the gums and supporting tissues)</li>
+                            <li>Oral implants and oral surgery</li>
+                            <li>Orthodontics</li> 
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Emergency Medicine</h1>
+                            <b>The Cardinal Santos Medical Center Emergency Room is under the auspices of the Department of Emergency Medicine.</b>
+                            <b>Led by top Philippine emergency medicine specialists, the Emergency Department Team of well-trained medical professionals is well equipped to provide emergent care to distressed patients, even those suffering from trauma and critical illness, any time of any day, 24/7. You can be sure of prompt and personal care and attention that any patient, young or old, critical or non-critical, deserves.</b>
+                            <br>
+                            <b><strong>For further details, please call (02) 727-0052 and we will be glad to assist you.</strong></b>       
+                    </div> 
+                    <div data-role="collapsible">     
+                        <h1>Internal Medicine</h1>
+                            <b>Cardinal Santos Medical Center is the preferred place of practice of some of the Philippines’ top practitioners in Internal Medicine. These specialists provide expert care for work-related conditions and injuries. They provide expertise in the prevention of such conditions to promote optimal health and safety in the workplace so you can come home at your best.</b>
+                            <b>The largest among the medical departments, the Department of Internal Medicine is composed of over 300 of the Philippines’ top specialists dealing with non-surgical diseases among adolescents and adults. It encompasses the following disciplines:</b> 
+                            <li>Allergy and Immunology</li>
+                            <li>Cardiology</li>
+                            <li>Dermatology</li>
+                            <li>Endocrinology and Diabetology</li>
+                            <li>Gastroenterology and Gastrointestinal Endoscopy</li>
+                            <li>General Internal Medicine</li>
+                            <li>Geriatric Medicine</li>
+                            <li>Hematology</li>
+                            <li>Infectious Diseases and Infection Control and Prevention</li>
+                            <li>Medical Oncology</li>
+                            <li>Nephrology and Hemodialysis</li>
+                            <li>Neurology</li>
+                            <li>Pulmonology</li>
+                            <li>Psychiatry</li>
+                            <li>Rheumatology</li>
+                            <li>Toxicology and Pharmacology</li></br> 
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Women’s Care</h1>
+                            <b>The Cardinal Santos Medical Center Women's Care is designed as a one-stop hub for various out-patient procedures and services especially catered to women. The unit provides women the necessary care needed to maintain good health and fitness.</b>  
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Endoscopy Unit</h1>
+                            <b>The Endoscopy Unit of Cardinal Santos Medical Center is dedicated to the assessment and treatment of the interior surfaces of the gastrointestinal tract through endoscopy. Minimal invasive techniques through allows for shorter hospital stay and the absence of surgical scars.</b> 
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Eye Center</h1>
+                            <b>The CSMC Eye Center offers expertise in multiple eye specialties for diagnosing and treating eye diseases. <br>
+                            <img src="images/diagnostic/EyeCenter.jpg" width="250px"></img>
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Hemodialysis</h1> 
+                            <b>The Hemodialysis specializes in caring for patients with kidney failure and those who need renal replacement therapy. The hemodialysis team aims to ensure utmost comfort and safety for its patients.</b>
+                            <img src="images/diagnostic/Hemodialysis.jpg" width="250px"></img>
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Laboratory</h1>
+                            <b>The Laboratory Department is equipped with up-to-date laboratory equipment for fast and accurate results.<br>
+                            <img src="images/diagnostic/PathologyandLaboratory.jpg" width="250px"></img>
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Pharmacy </h1>
+                            <b>The Pharmacy Department is responsible in providing optimal pharmaceutical care to patients with 24-hour service.<br>
+                            <img src="images/diagnostic/PharmacyUnit.jpg" width="250px"></img>
+                    </div>
+                    <div data-role="collapsible">
+                        </h1>Radiology</h1>
+                            <b>The Radiology Department offers a wide range of radiological procedures, using state-of-the-art imaging technology and performed by well-trained technologists, nurses and radiologists to ensure accuracy and safety of procedures.</b>
+                            <img src="images/diagnostic/RadiologyDepartment.jpg" width="250px"></img>   
+                        </div>
+                    <div data-role="collapsible">
+                        <h1>Rehabilitation Medicine</h1>
+                            <b>The Department of Rehabilitation Medicine specializes in treating neck and low back pain, tendonitis and arthritis; work and sports-related injuries; patients recovering from stroke; brain and spinal cord injury; patients rehabilitating from joint replacement surgery, among others.</b>
+                    </div>
+                    <div data-role="collapsible">
+                        <h1>Stone Center</h1>
+                            <b>The Stone Center also offers specialized, minimally invasive urologic surgeries with minimal or no skin incisions.</b> 
+                    </div>
+                </div>
+            </div>    
+            <div data-role="main" class="ui-content">
+                <button onclick="goBack()">Go Back</button>
+                    <script>
+                        function goBack() {
+                        window.history.back();
+                    }
+                    </script>      
+            </div>
+            </div>  
+        <aside class="bg-primary" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-dm-offset-2 text-center">
+                        <h2 class="section-heading">Contact Us!</h2>
+                        <p>
+                            Facebook:
+                        
+                            CardinalSantos          
+                        </p>
+                    </div>
+                    <div class="col-xs-6 col-dm-offset-2 text-center">
+                        <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                        <p>(+632) 727 0001</p>
+                    </div>
+                    <div class="col-xs-6 col-dm-offset-0 text-center">
+                        <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                        <p>info@csmc.ph</p>
+                    </div>
+                </div>
+        </div>
+        </aside
+        <script src="includes/jquery-1.7.2.min.js"></script>
+        <script src="includes/jquery.mobile-1.1.1/jquery.mobile-1.1.1.min.js"></script> 
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script> 
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script> 
+        <!-- Plugin JavaScript -->
+        <script src="js/jquery.easing.min.js"></script>
+        <script src="js/jquery.fittext.js"></script>
+        <script src="js/wow.min.js"></script> 
+        <!--Custom Theme JavaScript--> 
+        <script src="js/creative.js"></script> 
+    </body>
+    </html>
+<?php?>
---
+++ wp-content/themes/wpbootstrap/footer.php
@@ -0,0 +1,38 @@
+  <div class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2 class="section-heading">Cardinal Santos Medical Center:</h2> 
+                <p> 
+                	Address:
+					10 Wilson Street, Greenhills West
+					1502 San Juan City
+					Philippines 
+                </p> 
+            </div>
+        </div>
+    </div> 
+    <aside class="bg-primary" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-dm-offset-2 text-center">
+                    <h2 class="section-heading">Contact Us!</h2>
+                    <p>
+                 		Facebook:
+                    
+                    	CardinalSantos          
+                    </p>
+                </div>
+                <div class="col-xs-6 col-dm-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                    <p>(+632) 727 0001</p>
+                </div>
+                <div class="col-xs-6 col-dm-offset-0 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                    <p>info@csmc.ph</p>
+                </div>
+            </div>
+        </div>
+    </aside>  
+<?php wp_footer(); ?>
+</body> 
+</html>    
