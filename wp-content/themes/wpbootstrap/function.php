07213d5397b54de4a3ca14bd8b26de0949e8ec9f
Joshua Arenas <joshuaarenas820@gmail.com>
2016-06-16 01:34:20 +0800

    added diagnostic and special services

--- wp-content/themes/wpbootstrap/function.php
+++ wp-content/themes/wpbootstrap/function.php
@@ -1,20 +1,31 @@
 <?php 
 
+#
+
 function wpbootstrap_scripts_with_jquery()
 {
 	// Register the script like this for a theme:
+	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ) );
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/bootstrap.js', array( 'jquery' ) );
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/includes/jquery-1.7.2.min.js', array( 'jquery' ) );
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/includes/jquery.mobile-1.1.1/jquery.mobile-1.1.1.min.js', array( 'jquery' ) );
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/jquery.js', array( 'jquery' ) );
-	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/jsjquery.easing.min.js', array( 'jquery' ) );
+	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/jquery.easing.min.js', array( 'jquery' ) );
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/jquery.fittext.js', array( 'jquery' ) );
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/wow.min.js', array( 'jquery' ) );
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/creative.js', array( 'jquery' ) );
+
 	// For either a plugin or a theme, you can then enqueue the script:
 	wp_enqueue_script( 'custom-script' );
 }
 add_action( 'wp_enqueue_scripts', 'wpbootstrap_scripts_with_jquery' );
+
+//navigation theme setup
+
+
+// Register custom navigation walker
+
+
  
 /**
  * Register our sidebars and widgetized areas.
